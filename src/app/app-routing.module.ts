d66599419ce93b92df896216b2f02ccb6e0cdd27
RansAlexander <r0808003@student.thomasmore.be>
2023-01-16 14:41:14 +0100

    werf templates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { SiteDetailComponent } from './site-detail/site-detail.component';
+import { SitesComponent } from './sites/sites.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: AppComponent},
+  {path: 'sites', component: SitesComponent},
+  {path: 'sites/details', component: SiteDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
