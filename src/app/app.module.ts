2ddfaa7b99b06994458537d5f79f50544c6a43c8
Ruben Van de Wouwer <r0842395@student.thomasmore.be>
2023-01-16 10:54:23 +0100

    aanmaken components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SitesComponent } from './sites/sites.component';
+import { SiteComponent } from './site/site.component';
+import { SiteDetailComponent } from './site-detail/site-detail.component';
+import { PumpComponent } from './pump/pump.component';
+import { PumpDetailComponent } from './pump-detail/pump-detail.component';
+import { LogComponent } from './log/log.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SitesComponent,
+    SiteComponent,
+    SiteDetailComponent,
+    PumpComponent,
+    PumpDetailComponent,
+    LogComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/pump-detail/pump-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pump-detail',
+  templateUrl: './pump-detail.component.html',
+  styleUrls: ['./pump-detail.component.css']
+})
+export class PumpDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
