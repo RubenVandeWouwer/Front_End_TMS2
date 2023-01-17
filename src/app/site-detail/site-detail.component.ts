624c527f3481803caa07ba431811a0c0f9a21b53
Ruben Van de Wouwer <r0842395@student.thomasmore.be>
2023-01-17 15:09:56 +0100

    detail page site

--- src/app/site-detail/site-detail.component.ts
+++ src/app/site-detail/site-detail.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
+import { Site } from '../models/site';
+import { SiteService } from '../site.service';
 
 @Component({
   selector: 'app-site-detail',
   templateUrl: './site-detail.component.html',
-  styleUrls: ['./site-detail.component.css']
+  styleUrls: ['./site-detail.component.css'],
 })
 export class SiteDetailComponent implements OnInit {
+  site!: Site;
 
-  constructor() { }
+  constructor(
+    private siteService: SiteService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const siteId = this.route.snapshot.paramMap.get('id');
+    console.log(siteId);
+    if (siteId != null) {
+      this.siteService
+        .getSiteById(+siteId)
+        .subscribe((result) => (this.site = result));
+    }
   }
-
 }
