e42c46189c2054799261348b7359e6d02ed79c9f
Jeremy Coorevits <71607941+Jcoorevits@users.noreply.github.com>
2023-02-02 15:08:31 +0100

    production

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   firebase: {
 
     apiKey: "AIzaSyAXvmj1iA_wK0pBJBqx206zKhmX9eD9beg",
